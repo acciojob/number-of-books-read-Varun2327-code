60db7a83fd16c1318c9e39d73fe40a5c5e2dd6f2
Varun shrimal <varunshrimal07@gmail.com>
2025-05-15 20:48:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
+function numberOfBooksRead(library) {
+    let count = 0;
+    for (let book of library) {
+        if (book.readingStatus === true) {
+            count++;
+        }
+    }
+    return count;
+}
